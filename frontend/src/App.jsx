9222569225931b6dca14336614a140f0f7bd6fa9
Gaurav Gunjal <gauravgunjal45@gmail.com>
2026-06-13 22:11:50 +0530

    axiosClient for register, login, logout, etc.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,19 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { checkAuth } from "./authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-function App(){
+function App() {
+
+  const { isAuthenticated } = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [isAuthenticated]);
+
   return (
     <>
       <Routes>
